a02646258148c0a9ca621eb4e7f0f1276046cadf
michelleg27 <mgomez48@mail.csuchico.edu>
2018-02-01 11:45:38 -0800

    New Story

--- madlibs.cpp
+++ madlibs.cpp
@@ -4,6 +4,25 @@
 
 int main()
 {
+std::string adj; 
+char Let;
+int num; 
+std::string adj2; 
+
+std::cout<<"Adjective\n";
+std::cin>>adj; 
+std::cout<<"Letter\n"; 
+std::cin>>Let; 
+std::cout<<"Number\n"; 
+std::cin>>num; 
+std::cout<<"Adjective 2\n"; 
+std::cin>>adj2; 
+
+std::cout<<"Once upon a time there was a "<<adj<<" princesss.\n"; 
+std::cout<<"Her favorite thing to do was to go around the castle\n";
+std::cout<<"and turn anything that starts with the letter "<<Let<<" into a cat.\n";
+std::cout<<"At the end of the day she had "<<num<<" new cats.\n";
+std::cout<<"Her parents were very "<<adj2<<".\n";
 
   return 0;
 }
